f8e10c7b2d05860a507c49b956b9442b798d05ab
React Learning <amwilson2019@gmail.com>
2019-07-30 11:32:00 -0400

    form complete  and working on the login

--- haircare/src/App.js
+++ haircare/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import User from './components/UserRegistration';
-
+import Login from './components/Login';
+import Blank from './components/Blank';
+import {Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <User />
+      {/* <User /> */}
+      <Login />
     </div>
   );
 }
--- haircare/src/components/UserRegistration.js
+++ haircare/src/components/UserRegistration.js
@@ -1,51 +1,58 @@
-import React, {useState, useEffect} from 'react';
-
-const User = ()=> {
-const [registration, setRegistration] = useState({
-first_name: "",
-last_name: "",
-username: "",
-email: "",
-password: "",
-registrationErrors: ""
-});
-
-const handleChage = (e)=> {
-    const {name, value} = e.target;
-  console.log(name);
-  console.log(value);
-  setRegistration({...registration,
-    [name]: value});
-}
+import React, {useState} from 'react';
+const User = (props)=> {
+        const [registration, setRegistration] = useState({
+            first_name: "",
+            last_name: "",
+            username: "",
+            email: "",
+            password: "",
+            registrationErrors: ""
+            });
+        const handleChange = (e)=> {
+            const {name, value} = e.target;
+          console.log(name);
+          console.log(value);
+          setRegistration({...registration,
+            [name]: value});
+        }
+        const handleSubmit = (e)=> {
+            e.preventDefault();
+            setRegistration({...registration})
+        }    
+ 
     return (
         <div>
             <h1>User Registration</h1>
-            <form noValidate>
+            <form noValidate onSubmit={handleSubmit}>
                 <fieldset>
                     <label htmlFor="">
                         First Name:
-                        <input type='text' name='first_name' value={registration.first_name} onChange={handleChage} />
+                        <input type='text' name='first_name' value={registration.first_name} onChange={handleChange} />
                     </label>
                     <label htmlFor="">
                         Last Name:
-                        <input type='text' name='last_name' value={registration.last_name} onChange={handleChage} />
+                        <input type='text' name='last_name' value={registration.last_name} onChange={handleChange} />
                     </label>
                     <label htmlFor="">
                         Username:
-                        <input type='text' name='username' value={registration.username} onChange={handleChage}/>
+                        <input type='text' name='username' value={registration.username} onChange={handleChange}/>
                     </label>
                     <label htmlFor="">
                         Email:
-                        <input type='email' name='email' value={registration.email} onChange={handleChage} />
+                        <input type='email' name='email' value={registration.email} onChange={handleChange} />
                     </label>
                     <label htmlFor="">
                         Password:
-                        <input type='password' name='password' value={registration.password} onChange={handleChage} />
+                        <input type='password' name='password' value={registration.password} onChange={handleChange} />
                        {/*  Error message */}
                     </label>
                     <button type='submit'>Sign Up</button>
                 </fieldset>
             </form>
+            <h3>{registration.first_name}</h3>
+            <h3>{registration.last_name}</h3>
+            <h3>{registration.username}</h3>
+            <h3>{registration.email}</h3>
         </div>
     );
 }
